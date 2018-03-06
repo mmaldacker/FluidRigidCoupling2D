9079f577dafcd30cc75997430ce6db6bed92e218
Maximilian Maldacker <mmaldacker@gmail.com>
2018-03-06 21:00:47 +0100

    minor formatting

--- fluidsim.cpp
+++ fluidsim.cpp
@@ -675,22 +675,23 @@ void FluidSim::solve_pressure(float dt) {
     if (rbd)
     {
         rbd->getCOM(centre_of_mass);
-    }
-    for (int j = 0; j != nj; ++j) {
-        for (int i = 0; i != ni; ++i) {
-            double u_term = (rigid_u_weights(i + 1, j) - rigid_u_weights(i, j)) / dx;
-            double v_term = (rigid_v_weights(i, j + 1) - rigid_v_weights(i, j)) / dx;
 
-            // Translation coupling
-            base_trans_x(i, j) = u_term;
-            base_trans_y(i, j) = v_term;
+	    for (int j = 0; j != nj; ++j) {
+	        for (int i = 0; i != ni; ++i) {
+	            double u_term = (rigid_u_weights(i + 1, j) - rigid_u_weights(i, j)) / dx;
+	            double v_term = (rigid_v_weights(i, j + 1) - rigid_v_weights(i, j)) / dx;
 
-            // Rotation coupling
-            Vec2f position((i + 0.5f) * dx, (j + 0.5f) * dx);
-            Vec2f rad = position - centre_of_mass;
-            base_rot_z(i, j) = rad[0] * v_term - rad[1] * u_term;
-        }
-    }
+	            // Translation coupling
+	            base_trans_x(i, j) = u_term;
+	            base_trans_y(i, j) = v_term;
+
+	            // Rotation coupling
+	            Vec2f position((i + 0.5f) * dx, (j + 0.5f) * dx);
+	            Vec2f rad = position - centre_of_mass;
+	            base_rot_z(i, j) = rad[0] * v_term - rad[1] * u_term;
+	        }
+	    }
+	}
 
     int ni = v.ni;
     int nj = u.nj;
